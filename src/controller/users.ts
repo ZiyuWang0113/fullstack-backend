63758cf5106e03c81807c44ede896e70cea02f58
ZiyuWang0113 <ziw0372@gmail.com>
2025-07-03 00:52:06 -0400

    update login

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -2,48 +2,69 @@ import { Request, Response } from "express";
 import prisma from "../prismaClient";
 import bcrypt from "bcrypt";
 
-export const createUser = async (request: Request, response: Response) => {
-    const { name, email, password, roleId } = request.body;
-    await prisma.user.create({
+export const createUser = async (request:Request, response: Response) => {
+    const { name, email, password, role_id } = request.body;
+    const user = await prisma.user.create({
         data: {
             name: name,
             email: email,
-            role: {connect: { id: roleId }},
-            password: {create: {
-                hash: bcrypt.hashSync(password, 10)
-            }},
+            role: {
+                connect: {
+                    id: role_id
+                }
+            },
+            password: {
+                create: {
+                    hash: bcrypt.hashSync(password, 10)
+                }
+            }
         }
     })
-    response.json({ message: "User created successfully" });
+    response.status(201).json(user);
 }
+
 
 export const updateUser = async (request: Request, response: Response) => {
     const { id } = request.params;
     const user = await prisma.user.findUnique({
         where: {
-            id: Number(id)
+            id: Number(id),
+
         }
     })
-    if (!user) {
-        response.status(404).send({ message: "User not found" });
-        return;
+
+    if(!user) {
+        response.status(404).send('User not found');
+        return
     }
 
-    await prisma.user.update({
+    const updatedUser = await prisma.user.update({
         where: {
-            id: Number(id),
+            id: Number(id)
         },
         data: {
             name: request.body.name || user.name,
             email: request.body.email || user.email,
-            role: {connect: { id: request.body.roleId || user.role_id }},
+            role: {
+                connect: {
+                    id: request.body.role_id || user.role_id
+                }
+            }
         }
-    });
-    response.json({ message: "User updated successfully" });
+    })
+    response.json(updatedUser);
 }
 
+
 export const listUsers = async (request: Request, response: Response) => {
-    const users = await prisma.role.findMany();
+    const users = await prisma.user.findMany({
+        include: {
+            role: true
+        },
+        orderBy: {
+            id: 'asc'
+        }
+    });
     response.json(users);
 }
 
@@ -56,9 +77,10 @@ export const getUser = async (request: Request, response: Response) => {
     })
 
     if (!user) {
-        response.status(404).send({ message: "User not found" });
-        return;
+        response.status(404).send('User not found');
+        return
     }
+
     response.json(user);
 }
 
@@ -69,18 +91,15 @@ export const deleteUser = async (request: Request, response: Response) => {
             id: Number(id)
         }
     })
-    if (!user) {
-        response.status(404).send({ message: "User not found" });
-        return;
+    if(!user) {
+        response.status(404).send('User not found');
+        return
     }
 
     await prisma.user.delete({
         where: {
             id: Number(id)
         }
-    });
-
-    response.json({ message: "User deleted successfully" });
-}
-
-
+    })
+    response.json({ message: 'User deleted successfully' });
+}   
