5edeadac49231112afbcfb69f08ed10699ad3148
ZiyuWang0113 <ziw0372@gmail.com>
2025-07-06 11:46:56 -0400

    supabase deploy

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,23 @@ import rolesRouter from "./routes/roles";
 import usersRouter from "./routes/users";
 import authRouter from "./routes/auth";
 import { authentication } from "./middlewares/authentication";
-import productRouter from "./routes/products";
-import orderRouter from "./routes/order";
-
+import productsRouter from "./routes/products";
+import ordersRouter from "./routes/orders";
+import cors from "cors";
 const app = express();
 const bodyParser = require('body-parser')
+const port = process.env.PORT || 8888 
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(authentication)
 
 app.use(rolesRouter);
 app.use(usersRouter);
 app.use(authRouter);
-app.use(productRouter);
-app.use(orderRouter);
+app.use(productsRouter);
+app.use(ordersRouter);
 
-app.listen(8888, () => {
-    console.log('Server is running on port 8888')
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
